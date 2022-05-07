ecaa1c4ad0bff5780d8c925ab615ea1dbc5d95b5
Monarch <vamsivishwanath@gmail.com>
2022-05-07 16:53:40 +0530

    vamsu

---
+++ components/DashboardMainPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DashboardMainPage = () => {
+  return <div className="w-[70%] bg-blue-400"></div>;
+};
+
+export default DashboardMainPage;
---
+++ pages/dashboard/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DashboardMainPage from "../../components/DashboardMainPage";
+import DashboardSideBar from "../../components/DashboardSideBar";
+
+function index() {
+  return (
+
+  <DashboardSideBar />
+  <DashboardMainPage/>
+  )
+}
+
+export default index;
