9132236733b047641d192bac73656e88ce4b34cb
Monarch <vamsivishwanath@gmail.com>
2022-05-08 17:02:47 +0530

    phone banner modified

--- components/sections/LandingInfo.jsx
+++ components/sections/LandingInfo.jsx
@@ -6,7 +6,7 @@ const LandingInfo = () => {
         <div className="w-full flex overflow-x-hidden">
             <div className="w-full overflow-x-hidden">
                 <img
-                    src="/static/images/banner_image_w_phone.png"
+                    src="/static/images/banner_image_w_phone_1.png"
                     alt=""
                     className="w-full min-w-[500px] left-1/2 relative -translate-x-1/2"
                 />
